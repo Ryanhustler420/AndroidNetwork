3ff80871390b5366709cc6305b5c0aa669fbe69a
Gaurav Gupta <gouravgupta840@gmail.com>
2019-08-05 16:03:27 +0530

    Fetching Json Array From API

--- app/app/src/main/java/com/example/volley/MainActivity.java
+++ app/app/src/main/java/com/example/volley/MainActivity.java
@@ -2,6 +2,17 @@ package com.example.volley;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +22,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        // will get a Json Array
+        JsonArrayRequest arrayRequest = new JsonArrayRequest(Url, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                Log.d("Response: ", response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d("Error: ", error.getMessage());
+            }
+        });
+
+        // we have to add request to queue
+        queue.add(arrayRequest);
     }
 }
